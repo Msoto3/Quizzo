1feda21f0cfe7c36c8fea6f3dd0ad2c238335873
Rayo Graham <Yibran Calidonio>
2023-12-02 15:52:41 -0500

    Changed Server.py code so it would work on current openAI version, Moved custom to top, added another ai prompt so no repeats happen in right answer

--- backend/server.py
+++ backend/server.py
@@ -9,16 +9,18 @@ apikey = os.getenv('API_KEY')
 app = Flask(__name__)
 CORS(app)
 
+
 # Set your API key
 @app.route("/<genre>")
 def quiz(genre):
+    
     openai.api_key = apikey
-
+    
     # Define a prompt for ChatGPT
     prompt = f"give me 4 simple questions about {genre}"
     # Call the OpenAI API to generate a response
-    response = openai.Completion.create(
-        engine="text-davinci-003",  # Choose an appropriate engine (check the latest options in OpenAI's documentation)
+    response = openai.completions.create(
+        model="text-davinci-003",  # Choose an appropriate engine (check the latest options in OpenAI's documentation)
         prompt=prompt,
         max_tokens=60,  # You can adjust the response length
     )
@@ -36,30 +38,39 @@ def quiz(genre):
 
     for question in new_list:
         # gets the right answer
-        res = openai.Completion.create(
-            engine="text-davinci-003",  # Choose an appropriate engine (check the latest options in OpenAI's documentation)
+        res = openai.completions.create(
+            model="text-davinci-003",  # Choose an appropriate engine (check the latest options in OpenAI's documentation)
             prompt=f'{question}. Give only 1 answer, make it simple meaning make it short and do not make it longer than 20 tokens, and do not describe the answer.',
             max_tokens=20,  # You can adjust the response length
         )
         right_answer = res.choices[0].text.replace('\n','').replace('?','')
 
         #gets the wrong answers
-        res = openai.Completion.create(
-            engine="text-davinci-003",  # Choose an appropriate engine (check the latest options in OpenAI's documentation)
+        res = openai.completions.create(
+            model="text-davinci-003",  # Choose an appropriate engine (check the latest options in OpenAI's documentation)
             prompt=f'{question}. give 3 wrong answers only, also please seperate them by a comma, do not specify that they are wrong answer, do not number them, do not make them longer than 30 tokens and they cannot be the same as {right_answer}.',
             max_tokens=30,  # You can adjust the response length
         )
         wrong_answers = res.choices[0].text.replace('\n','').replace('?','').split(",")
         wrong_answers = [x.strip() for x in wrong_answers if x]
 
-        res = openai.Completion.create(
-            engine="text-davinci-003",  # Choose an appropriate engine (check the latest options in OpenAI's documentation)
-            prompt=f'Generate a hint for {question}. Make it subtle and short to quide the quiz and keep it no onger than 30 tokens, and do not repeat this {right_answer}.',
+        res = openai.completions.create(
+            model="text-davinci-003",  # Choose an appropriate engine (check the latest options in OpenAI's documentation)
+            prompt=f'Based off this question: {question}, give 1 answer, make it simple, do not describe the answer and it cannot be the same as these wrong asnwers: {wrong_answers}',
+            max_tokens=20,  # You can adjust the response length
+        )
+        right_answer = res.choices[0].text.replace('\n','').replace('?','')
+
+
+
+        res = openai.completions.create(
+            model="text-davinci-003",  # Choose an appropriate engine (check the latest options in OpenAI's documentation)
+            prompt=f'Generate a hint for {question}. Make it subtle and short to quide the quiz and keep it no longer than 30 tokens, and  cannot be the same as this right answer: {right_answer}.',
             max_tokens=30,  # You can adjust the response length
         )
         hint = res.choices[0].text.replace('\n','').replace('?','')
         qna[question]={"correct":right_answer,"incorrect":wrong_answers,"hint" : hint}
-
+     
     return(qna)
 
 if __name__== "__main__":
--- frontend/src/components/Start.js
+++ frontend/src/components/Start.js
@@ -18,46 +18,47 @@ export default function Start ({setStart,setDest}){
             <h1>Quizzo</h1>
         </header>
         <br/>
-        <div className="center-container"> 
+        <div className="center-container">
             <div className="grid-container">
-            {/* Row 1 */}
-            <div onClick={()=>{setDest("science");setStart(true)}} className="grid-item" id="item1">
-                Science
-            </div>
-            <div onClick={()=>{setDest("math");setStart(true)}} className="grid-item" id="item2">
-                Math
-            </div>
-            <div onClick={()=>{setDest("sports");setStart(true)}} className="grid-item" id="item3">
-                Sports
-            </div>
-
-            {/* Row 2 */}
-            <div onClick={()=>{setDest("popculture");setStart(true)}} className="grid-item" id="item4">
-                Pop Culture
-            </div>
-            <div onClick={()=>{setDest("history");setStart(true)}} className="grid-item" id="item5">
-                History
-            </div>
-            <div onClick={()=>{setDest("animals");setStart(true)}} className="grid-item" id="item6">
-                Animals
-            </div>
-
-            <div className="grid-item" id="item7">
+                {/* Row 0 */}
+                <div className="grid-item" id="item7">
                 Custom Prompt
                 <input
-                type="text"
-                value={userInput}
-                placeholder="Type Genre Here"
-                style={{ marginRight: '10px' }}
-                onChange={handleInputChange}
+                    type="text"
+                    value={userInput}
+                    placeholder="Type Genre Here"
+                    style={{ marginRight: '10px' }}
+                    onChange={handleInputChange}
                 />
+                    </div>
+                <div onClick={userPrompt} className="grid-item" id="item8">
+                {/* Content for item8 */}
+                Click this grid to begin custom quiz
                 </div>
-            <div onClick={userPrompt} className="grid-item" id="item8">
-            </div>
-            <div className="grid-item" id="item7">
-            </div>
+                {/* Row 1 */}
+                <div onClick={() => { setDest("science"); setStart(true) }} className="grid-item" id="item1">
+                Science
+                </div>
+                <div onClick={() => { setDest("math"); setStart(true) }} className="grid-item" id="item2">
+                Math
+                </div>
+                <div onClick={() => { setDest("sports"); setStart(true) }} className="grid-item" id="item3">
+                Sports
+                </div>
+
+                {/* Row 2 */}
+                <div onClick={() => { setDest("popculture"); setStart(true) }} className="grid-item" id="item4">
+                Pop Culture
+                </div>
+                <div onClick={() => { setDest("history"); setStart(true) }} className="grid-item" id="item5">
+                History
+                </div>
+                <div onClick={() => { setDest("animals"); setStart(true) }} className="grid-item" id="item6">
+                Animals
+                </div>
             </div>
         </div>
+
         </>
     )
 }
