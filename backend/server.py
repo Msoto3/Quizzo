1e2d835ceed2aeb93812adb11e7417de996b4ddb
Your Name <matthewsoto85@gmail.com>
2023-12-19 12:40:08 -0500

    Revert "reverted changes"
    This reverts commit c64a254554d4b9a6cdb222564af43e7302e7246f.

--- backend/server.py
+++ backend/server.py
@@ -15,6 +15,7 @@ MAX_VALIDATION_TRIES = 3  # Maximum attempts to validate the right answer
 @app.route("/<genre>/<numberofquestions>")
 def quiz(genre, numberofquestions):
     openai.api_key = apikey
+    
     prompt = f"give me {numberofquestions} simple questions about \"{genre}\""
     response = openai.Completion.create(
         model="text-davinci-003",
